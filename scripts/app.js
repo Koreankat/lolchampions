16b1bb57256c1255eb03cbd3459f7f1a4e1c3d69
korean_kat <katarina999@outlook.fr>
2022-02-01 00:59:44 +0100

    adding for loop for icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,48 +1,47 @@
-const getVal= () =>{ 
-  
-  
-    let txt = document.getElementById("search-box").value;
-   const txt1 = txt.toLowerCase();
-   const X = txt1.charAt(0).toUpperCase()+txt1.slice(1);
-   async function createPage() {
-  let response = await fetch(
-    `https://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion/${X}.json`,
-    {
-      method: "GET",
+const getVal = () => {
+  const Q = document.getElementById("iconone")
+  const W = document.getElementById("icontwo")
+  const E = document.getElementById("iconthree")
+  const R = document.getElementById("iconfour")
+  const P = document.getElementById("iconfive")
+  const abilities = [Q, W, E, R]
+  const txt = document.getElementById("search-box").value
+  const txt1 = txt.toLowerCase()
+  const X = txt1.charAt(0).toUpperCase() + txt1.slice(1)
+
+  async function createPage() {
+    let response = await fetch(
+      `https://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion/${X}.json`,
+      {
+        method: "GET",
+      }
+    )
+    let data = await response.json()
+
+    document.getElementById("info").style.display = ""
+
+    const spellsId = data.data[X].spells
+    const passive = data.data[X].passive.image.full
+
+    for (let i = 0; i < spellsId.length; i++) {
+      let spells = data.data[X].spells[i].image.full
+      abilities[
+        i
+      ].style.background = `url(https://ddragon.leagueoflegends.com/cdn/12.2.1/img/spell/${spells})`
     }
-  )
-  let data = await response.json();
 
- document.getElementById("info").style.display='';
- 
-        let spellQ = data.data[X].spells[0].image.full;
-        let spellW = data.data[X].spells[1].image.full;
-        let spellE = data.data[X].spells[2].image.full;
-        let spellR = data.data[X].spells[3].image.full;
-        let passive = data.data[X].passive.image.full;
+    P.style.background = `url(https://ddragon.leagueoflegends.com/cdn/12.2.1/img/passive/${passive})`
 
-  document.getElementById("iconone").style.background= `url(https://ddragon.leagueoflegends.com/cdn/12.2.1/img/spell/${spellQ})`;
-  document.getElementById("icontwo").style.background= `url(https://ddragon.leagueoflegends.com/cdn/12.2.1/img/spell/${spellW})`;
-  document.getElementById("iconthree").style.background= `url(https://ddragon.leagueoflegends.com/cdn/12.2.1/img/spell/${spellE})`;
-  document.getElementById("iconfour").style.background= `url(https://ddragon.leagueoflegends.com/cdn/12.2.1/img/spell/${spellR})`;
-  document.getElementById("iconfive").style.background= `url(https://ddragon.leagueoflegends.com/cdn/12.2.1/img/passive/${passive})`;
-  
-
-     
-
-  document.getElementById("intro").innerHTML = `
+    document.getElementById("intro").innerHTML = `
       <h2>${data.data[X].title}</h2>
-      <h1>${data.data[X].name}</h1>`;
+      <h1>${data.data[X].name}</h1>`
 
-
-  
-
-  document.getElementById("general-info").innerHTML = `
+    document.getElementById("general-info").innerHTML = `
     <p style="color:lightgrey">Role:</p>
     <p>${data.data[X].tags}</p>
     <p style="color:lightgrey">Resource :</p>
-    <p>${data.data[X].partype}</p>`;
-  document.getElementById("base-stats").innerHTML = `
+    <p>${data.data[X].partype}</p>`
+    document.getElementById("base-stats").innerHTML = `
     <p style="color:lightgrey">Health :</p>
     <p>${data.data[X].stats.hp}</p>
     <p style="color:lightgrey">Health Regen :</p>
@@ -64,16 +63,7 @@ const getVal= () =>{
     <p style="color:lightgrey">Attack Speed :</p>
     <p>${data.data[X].stats.attackspeed}</p>
     <p style="color:lightgrey">Move Speed :</p>
-    <p>${data.data[X].stats.movespeed}</p>`;
-
-
-
-    }
-    createPage();
-
-  
-
+    <p>${data.data[X].stats.movespeed}</p>`
+  }
+  createPage()
 }
-
-    
- 
